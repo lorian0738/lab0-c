faf4c5f92f254d12002d6c06b2995b67ff3ced34
cyh <p76114383@gs.ncku.edu.tw>
2023-02-26 00:08:28 +0800

    Implement remove_tail

--- queue.c
+++ queue.c
@@ -97,7 +97,19 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *element = list_last_entry(head, element_t, list);
+
+    list_del(head->prev);
+
+    if (sp != NULL) {
+        strncpy(sp, element->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return element;
 }
 
 /* Return number of elements in queue */
