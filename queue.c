463f29ef0bb4bd0d673a5273a587c13f0e996f72
cyh <p76114383@gs.ncku.edu.tw>
2023-02-26 18:52:21 +0800

    Implement delete_mid

--- queue.c
+++ queue.c
@@ -144,7 +144,22 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
-    return true;
+    if (!head || list_empty(head))
+        return false;
+
+    int mid = q_size(head) / 2;
+
+    element_t *entry = NULL, *safe = NULL;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        if (mid == 0) {
+            list_del(&entry->list);
+            q_release_element(entry);
+            return true;
+        }
+        mid = mid - 1;
+    }
+
+    return false;
 }
 
 /* Delete all nodes that have duplicate string */
